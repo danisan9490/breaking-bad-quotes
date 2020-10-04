878dcd7b1666557a65375bfbd8741fa14e6d5d6a
Daniel <danisan9490@gmail.com>
2020-10-04 09:57:17 +0100

    Updates

---
+++ precache-manifest.3bb671d7e6c9a9ef110d2e88d096763c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bcfe22199d94e1d13bf3ce0943901a",
+    "url": "/breaking-bad-quotes/index.html"
+  },
+  {
+    "revision": "236046c65ed0c98f42ef",
+    "url": "/breaking-bad-quotes/static/css/main.5d6f989c.chunk.css"
+  },
+  {
+    "revision": "b5c6ee409fbaea82c7d3",
+    "url": "/breaking-bad-quotes/static/js/2.f9fe8298.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-quotes/static/js/2.f9fe8298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236046c65ed0c98f42ef",
+    "url": "/breaking-bad-quotes/static/js/main.14196e69.chunk.js"
+  },
+  {
+    "revision": "350c8e8849e59ca97063",
+    "url": "/breaking-bad-quotes/static/js/runtime-main.e6a235d4.js"
+  },
+  {
+    "revision": "21fb223be084d8c28fd19dbeb8522b88",
+    "url": "/breaking-bad-quotes/static/media/logo.21fb223b.svg"
+  }
+]);
